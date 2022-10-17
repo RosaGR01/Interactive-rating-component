ceec48cc806a7e83660cdc34a4e7d21c7f2fab1b
rosalba <rosa.galicia.ramos1998@gmail.com>
2022-10-17 11:30:05 -0500

    se agrego la funcionalidad con js

---
+++ main.js
@@ -0,0 +1,15 @@
+const btn = document.getElementById('btn');
+
+function seleccionado( num){
+    document.getElementById('texto').innerHTML = `You sected ${num} out of 5`;
+
+}
+
+function ponderacion(){
+    document.getElementById('calificar').classList.add('inactive');
+    document.getElementById('agradeser').classList.remove('inactive');
+
+
+    
+    
+}
