c0bc5b114c79b6bb989c9142fa93762d3836a8ad
rosalba <rosa.galicia.ramos1998@gmail.com>
2022-10-17 11:53:40 -0500

    modificaciones al diseño responsivo

--- main.js
+++ main.js
@@ -2,14 +2,9 @@ const btn = document.getElementById('btn');
 
 function seleccionado( num){
     document.getElementById('texto').innerHTML = `You sected ${num} out of 5`;
-
 }
 
 function ponderacion(){
     document.getElementById('calificar').classList.add('inactive');
     document.getElementById('agradeser').classList.remove('inactive');
-
-
-    
-    
 }
